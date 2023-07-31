e9bc5d41e4a3cd9ceb8348ec26c1623d76238d2a
Lennart Bank <contact@lencodes.com>
2023-07-31 15:18:09 +0200

    INIT

--- pwa/src/templates/templateParts/header/HeaderTemplate.tsx
+++ pwa/src/templates/templateParts/header/HeaderTemplate.tsx
@@ -2,11 +2,16 @@ import * as React from "react";
 import * as styles from "./HeaderTemplate.module.css";
 
 import { PageHeader } from "@utrecht/component-library-react/dist/css-module";
+import { navigate } from "gatsby";
 
 export const HeaderTemplate: React.FC = () => {
   return (
     <PageHeader>
-      <div className={styles.container}>Header</div>
+      <div className={styles.container}>
+        <span onClick={() => navigate("/")} className={styles.title}>
+          Wet open overheid (Woo)
+        </span>
+      </div>
     </PageHeader>
   );
 };
