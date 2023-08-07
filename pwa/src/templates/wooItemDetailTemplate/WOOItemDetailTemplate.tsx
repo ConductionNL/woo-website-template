9d0e767f9e4b6648823c78de93976bac4bbab81c
Remko <remko@conduction.nl>
2023-08-07 14:13:57 +0200

    cleanup

--- pwa/src/templates/wooItemDetailTemplate/WOOItemDetailTemplate.tsx
+++ pwa/src/templates/wooItemDetailTemplate/WOOItemDetailTemplate.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import * as styles from "./RequestDetailTemplate.module.css";
+import * as styles from "./WOOItemDetailTemplate.module.css";
 import {
   Page,
   PageContent,
