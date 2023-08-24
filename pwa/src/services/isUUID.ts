bc424ae54ecdba71bab621df0a157dcfbff2c030
Remko <remko@conduction.nl>
2023-08-24 13:46:41 +0200

    Added feedback

---
+++ pwa/src/services/isUUID.ts
@@ -0,0 +1,9 @@
+export const isUUID = (id: string): boolean => {
+  const regexExp = /^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/gi;
+
+  if (regexExp.test(id)) {
+    return true;
+  } else {
+    return false;
+  }
+};
