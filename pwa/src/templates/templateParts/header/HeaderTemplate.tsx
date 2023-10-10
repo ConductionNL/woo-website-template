5e3b09542c21b831f3bf61e5c16af1607b2e299c
Remko <remkohuisman@hotmail.nl>
2023-10-10 09:53:00 +0200

    added suggested changes

--- pwa/src/templates/templateParts/header/HeaderTemplate.tsx
+++ pwa/src/templates/templateParts/header/HeaderTemplate.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import * as styles from "./HeaderTemplate.module.css";
 import clsx from "clsx";
-import { PageHeader } from "@utrecht/component-library-react/dist/css-module";
+import { PageHeader, SkipLink } from "@utrecht/component-library-react/dist/css-module";
 import { navigate } from "gatsby";
 import { useTranslation } from "react-i18next";
 import { useGatsbyContext } from "../../../context/gatsby";
@@ -17,44 +17,43 @@ export const HeaderTemplate: React.FC<HeaderTemplateProps> = ({ layoutClassName 
   return (
     <PageHeader className={clsx(layoutClassName && layoutClassName)}>
       <div className={styles.container}>
-        <a
-          href="#filters"
-          className={styles.visuallyHidden}
-          tabIndex={gatsbyContext.location.pathname === "/" ? 0 : -1}
-        >
-          {t("Skip to filters")}
-        </a>
-        <a href="#mainContent" className={styles.visuallyHidden} tabIndex={0}>
-          {t("Skip to main content")}
-        </a>
-
-        <div className={styles.imageContainer}>
-          <img
-            className={styles.image}
-            onClick={() => navigate("/")}
-            src={process.env.GATSBY_HEADER_LOGO_URL}
-            alt={t("Navbar-Logo")}
-            tabIndex={0}
-          />
+        <div className={styles.skipLinkContainer}>
+          <SkipLink href="#filters" tabIndex={gatsbyContext.location.pathname === "/" ? 0 : -1}>
+            {t("Skip to filters")}
+          </SkipLink>
+          <SkipLink href="#mainContent" tabIndex={0}>
+            {t("Skip to main content")}
+          </SkipLink>
         </div>
-        <div className={styles.languageSelectContainer}>
-          <span
-            className={clsx(styles.languageSelect, i18n.language === "nl" && styles.languageSelectDisabled)}
-            onClick={() => i18n.changeLanguage("nl")}
-            tabIndex={0}
-            aria-label={t("Translate page to Dutch")}
-          >
-            NL
-          </span>{" "}
-          /{" "}
-          <span
-            className={clsx(styles.languageSelect, i18n.language === "en" && styles.languageSelectDisabled)}
-            onClick={() => i18n.changeLanguage("en")}
-            tabIndex={0}
-            aria-label={t("Translate page to English")}
-          >
-            EN
-          </span>
+        <div className={styles.navContainer}>
+          <div className={styles.imageContainer}>
+            <img
+              className={styles.image}
+              onClick={() => navigate("/")}
+              src={process.env.GATSBY_HEADER_LOGO_URL}
+              alt={t("Navbar-Logo")}
+              tabIndex={0}
+            />
+          </div>
+          <div className={styles.languageSelectContainer}>
+            <span
+              className={clsx(styles.languageSelect, i18n.language === "nl" && styles.languageSelectDisabled)}
+              onClick={() => i18n.changeLanguage("nl")}
+              tabIndex={0}
+              aria-label={t("Translate page to Dutch")}
+            >
+              NL
+            </span>{" "}
+            /{" "}
+            <span
+              className={clsx(styles.languageSelect, i18n.language === "en" && styles.languageSelectDisabled)}
+              onClick={() => i18n.changeLanguage("en")}
+              tabIndex={0}
+              aria-label={t("Translate page to English")}
+            >
+              EN
+            </span>
+          </div>
         </div>
       </div>
     </PageHeader>
