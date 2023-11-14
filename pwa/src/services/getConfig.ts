ea27f493dcb69f2d242de7eeec3b1f960c468fcb
Lencodes <contact@lencodes.com>
2023-11-14 12:01:32 +0100

    IMPLEMENT feedback and fallback theme

--- pwa/src/services/getConfig.ts
+++ pwa/src/services/getConfig.ts
@@ -1,8 +1,11 @@
+import Conduction from "./../../static/configFiles/conduction.json";
 import Epe from "./../../static/configFiles/epe.json";
 
 export const getConfig = (domain: string): Record<string, any> | undefined => {
   switch (domain) {
     case "open.epe.nl":
       return Epe as Record<string, any>;
+    default:
+      return Conduction as Record<string, any>;
   }
 };
