9a1def008a23d6f71c67fcc52a8a01d6576a9558
Remko <remko@conduction.nl>
2023-11-28 12:38:18 +0100

    WIP

--- pwa/src/templates/templateParts/cardsResultsTemplate/CardsResultsTemplate.tsx
+++ pwa/src/templates/templateParts/cardsResultsTemplate/CardsResultsTemplate.tsx
@@ -1,10 +1,12 @@
 import * as React from "react";
 import * as styles from "./CardsResultsTemplate.module.css";
+import clsx from "clsx";
 import { Heading2, Paragraph } from "@utrecht/component-library-react/dist/css-module";
 import { translateDate } from "../../../services/dateFormat";
 import { useTranslation } from "react-i18next";
 import { navigate } from "gatsby";
 import { CardHeader, CardHeaderDate, CardHeaderTitle, CardWrapper } from "@conduction/components";
+import { TOOLTIP_ID } from "../../../layout/Layout";
 
 interface CardsResultsTemplateProps {
   requests: any[];
@@ -12,6 +14,8 @@ interface CardsResultsTemplateProps {
 
 export const CardsResultsTemplate: React.FC<CardsResultsTemplateProps> = ({ requests }) => {
   const { t, i18n } = useTranslation();
+
+  const themesWithBefore = ["epe-theme"];
 
   return (
     <>
@@ -22,11 +26,24 @@ export const CardsResultsTemplate: React.FC<CardsResultsTemplateProps> = ({ requ
             className={styles.cardContainer}
             onClick={() => navigate(request._self.id)}
             tabIndex={0}
-            aria-label={`${request.titel}, ${request.samenvatting}, ${
+            aria-label={`${
               request.publicatiedatum ? translateDate(i18n.language, request.publicatiedatum) : t("N/A")
+            }, ${request.titel}, ${request.samenvatting} ${
+              window.sessionStorage.getItem("SHOW_ORGANIZATION") === "true"
+                ? `,${request.embedded?.behandelendBestuursorgaan?.naam}`
+                : ""
+            } ${
+              window.sessionStorage.getItem("SHOW_CATEGORY") === "true"
+                ? `, ${t("Category")}, ${request.categorie}`
+                : ""
             }`}
           >
-            <CardHeader className={styles.cardHeader}>
+            <CardHeader
+              className={clsx(
+                themesWithBefore.includes(window.sessionStorage.getItem("NL_DESIGN_THEME_CLASSNAME") ?? "") &&
+                  styles.cardHeader,
+              )}
+            >
               <CardHeaderDate>
                 {request.publicatiedatum ? translateDate(i18n.language, request.publicatiedatum) : t("N/A")}
               </CardHeaderDate>
@@ -36,6 +53,26 @@ export const CardsResultsTemplate: React.FC<CardsResultsTemplateProps> = ({ requ
             </CardHeader>
 
             <Paragraph className={styles.description}>{request.samenvatting}</Paragraph>
+
+            {(window.sessionStorage.getItem("SHOW_CATEGORY") === "true" ||
+              window.sessionStorage.getItem("SHOW_ORGANIZATION") === "true") && (
+              <div className={styles.cardFooter}>
+                {window.sessionStorage.getItem("SHOW_ORGANIZATION") === "true" && (
+                  <CardHeaderDate>
+                    <span data-tooltip-id={TOOLTIP_ID} data-tooltip-content={t("Municipality")}>
+                      {request.embedded?.behandelendBestuursorgaan?.naam}
+                    </span>
+                  </CardHeaderDate>
+                )}
+                {window.sessionStorage.getItem("SHOW_CATEGORY") === "true" && (
+                  <CardHeaderDate>
+                    <span data-tooltip-id={TOOLTIP_ID} data-tooltip-content={t("Category")}>
+                      {request.categorie}
+                    </span>
+                  </CardHeaderDate>
+                )}
+              </div>
+            )}
           </CardWrapper>
         ))}
       </div>
