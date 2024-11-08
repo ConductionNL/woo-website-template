f60d4455883f62a3ce7b00718f6750ec2facf390
Remko <remko@conduction.nl>
2024-11-08 15:56:20 +0100

    barneveld hotfix

--- pwa/src/services/getConfig.ts
+++ pwa/src/services/getConfig.ts
@@ -3,7 +3,7 @@ import Albrandswaard from "./../../static/configFiles/municipalities/albrandswaa
 import AlbrandswaardAccept from "./../../static/configFiles/municipalities/albrandswaard/albrandswaard-accept.json";
 import Barendrecht from "./../../static/configFiles/municipalities/barendrecht/barendrecht.json";
 import BarendrechtAccept from "./../../static/configFiles/municipalities/barendrecht/barendrecht-accept.json";
-import BarneveldAccept from "./../../static/configFiles/municipalities/barendrecht/barneveld-accept.json";
+import BarneveldAccept from "./../../static/configFiles/municipalities/barneveld/barneveld-accept.json";
 import Buren from "./../../static/configFiles/municipalities/buren/buren.json";
 import BurenAccept from "./../../static/configFiles/municipalities/buren/buren-accept.json";
 import Dinkelland from "./../../static/configFiles/municipalities/dinkelland/dinkelland.json";
